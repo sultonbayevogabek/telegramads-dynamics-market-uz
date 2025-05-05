0197be266d9a760866e5de338d98c596ef141d84
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-05-05 15:47:22 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/dildortursunovamasterklas';
+    window.location.href = 'https://t.me/+1nfE3YjeLz43Njky';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
